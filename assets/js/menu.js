9c7bbbbfb8ea9ed78b9d7739aef04e66104ee6d4
aldaydev <rafaldayparejo@gmail.com>
2024-10-04 04:02:09 +0200

    Empezando con el body: grid y contenido

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -6,19 +6,34 @@ document.addEventListener ("DOMContentLoaded" , ()=>{
     let line3 = document.querySelector(".toogle__line--3");
     let line4 = document.querySelector(".toogle__line--4");
 
-toggle.addEventListener("click", ()=>{
-    let visible = document.querySelector(".visible");
-    if(!visible) {
-        header.classList.add("visible");
-        header.style.height = "330px";
-        line1.style.top = "50%";
-        line1.style.opacity = "0";
-        line4.style.top = "50%";
-        line4.style.opacity = "0";
-        line2.style.transform = "rotate(45deg)";
-        line3.style.transform = "rotate(-45deg)";
+    toggle.addEventListener("click", ()=>{
+        let visible = document.querySelector(".visible");
+        if(!visible) {
+            header.classList.add("visible");
+            header.style.height = "330px";
+            line1.style.top = "50%";
+            line1.style.opacity = "0";
+            line4.style.top = "50%";
+            line4.style.opacity = "0";
+            line2.style.transform = "rotate(45deg)";
+            line3.style.transform = "rotate(-45deg)";
+            
+        }else{
+            header.classList.remove("visible");
+            header.style.height = "73.97px";
+            line1.style.top = "5px";
+            line1.style.opacity = "1";
+            line4.style.top = "25px";
+            line4.style.opacity = "1";
+            line2.style.transform = "rotate(0deg)";
+            line3.style.transform = "rotate(0deg)";
+        };
+    });
+
+    window.addEventListener("resize", ()=>{
+        //let size = parseInt(document.body.clientWidth);
         
-    }else{
+        //if (size !== size){
         header.classList.remove("visible");
         header.style.height = "73.97px";
         line1.style.top = "5px";
@@ -27,7 +42,7 @@ toggle.addEventListener("click", ()=>{
         line4.style.opacity = "1";
         line2.style.transform = "rotate(0deg)";
         line3.style.transform = "rotate(0deg)";
-    };
-});
+        //}
+    });
 
 });
